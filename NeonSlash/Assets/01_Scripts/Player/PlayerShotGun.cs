cee0e127fc0caf393cccfe3db176378f8b00f6c6
Kangyoongu <rkas7890@gmail.com>
2024-08-16 00:40:13 +0900

    merge1

--- NeonSlash/Assets/01_Scripts/Player/PlayerShotGun.cs
+++ NeonSlash/Assets/01_Scripts/Player/PlayerShotGun.cs
@@ -40,7 +40,7 @@ public class PlayerShotGun : MonoBehaviour
         for (int i = 0; i < _playerCompo.copyPlayerStat.playerStat.attackNum; i++)
         {
             Transform bullet = ObjectPool.Instance.GetPooledObject("Bullet").transform;
-            Vector3 spreadVec = Random.onUnitSphere * Random.Range(0f, spread) * Mathf.InverseLerp(1f, 50f, _playerCompo.copyPlayerStat.playerStat.attackNum);//나중에 최대 attackNum으로 변경
+            Vector3 spreadVec = Random.onUnitSphere * Random.Range(0f, spread) * Mathf.InverseLerp(1f, 6f, _playerCompo.copyPlayerStat.playerStat.attackNum);//나중에 최대 attackNum으로 변경
             bullet.SetPositionAndRotation(muzzle.position, muzzle.rotation * Quaternion.Euler(spreadVec));
             bullet.GetComponent<Bullet>().Init(_playerCompo.copyPlayerStat.playerStat.attackDis, _playerCompo.copyPlayerStat.playerStat.attack);
         }
